686e517a84b7d9d291c55856e10c0cd5786dc64e
Ivan Markovski <sagitariusim@gmail.com>
2024-05-08 08:16:35 +0200

    adding user handlers, updating models

--- internal/repository/migrations/000001_init_schema.up.sql
+++ internal/repository/migrations/000001_init_schema.up.sql
@@ -1,34 +1,50 @@
-create table if not exists public.employees
-(
-    id               uuid not null
-        primary key,
-    first_name       text,
-    last_name        text,
-    hashed_password  text,
-    current_position text,
-    position_id      uuid
-        constraint position_id
-            references public.positions
-);
-
-alter table public.employees
-    owner to postgres;
-
-
-
 create table if not exists public.positions
 (
-    id            uuid not null
+    id            bigserial
         primary key,
     position_name text,
-    employees     text[],
-    shifts        text[],
-    start_time    varchar(10),
-    end_time      varchar(10),
     created_at    timestamp,
     updated_at    timestamp
 );
 
 alter table public.positions
+    owner to postgres;
+
+create table if not exists public.users
+(
+    id               bigserial
+        primary key,
+    first_name       text,
+    last_name        text,
+    email            text,
+    password         text,
+    current_position text,
+    role             text,
+    created_at       timestamp,
+    updated_at       timestamp,
+    position_id      bigint not null
+        constraint fk_positions_users
+            references public.positions
+);
+
+alter table public.users
+    owner to postgres;
+
+create table if not exists public.shifts
+(
+    id          bigserial
+        primary key,
+    name        varchar(5),
+    start_time  timestamp with time zone,
+    end_time    timestamp with time zone,
+    position_id bigserial
+        constraint fk_positions_shifts
+            references public.positions,
+    user_id     bigserial
+        constraint fk_users_shifts
+            references public.users
+);
+
+alter table public.shifts
     owner to postgres;
 
