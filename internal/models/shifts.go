abc7d404644b166a6b50fd24c2546c6bb3e80459
Ivan Markovski <sagitariusim@gmail.com>
2024-05-21 22:01:14 +0200

    updating shifts datatype for db

--- internal/models/shifts.go
+++ internal/models/shifts.go
@@ -2,7 +2,7 @@ package models
 
 type Shifts struct {
 	ID         int64  `json:"id" db:"id" gorm:"primaryKey;type:bigint"`
-	Name       string `json:"name" db:"name" gorm:"type:varchar(5)"`
+	Name       string `json:"name" db:"name" gorm:"type:varchar(15)"`
 	StartTime  string `json:"start_time" db:"start_time" gorm:"type:time"`
 	EndTime    string `json:"end_time" db:"end_time" gorm:"type:time"`
 	PositionID int64  `json:"position_id" db:"position_id" gorm:"type:bigint"`
