3c6be8286e515820219eab791621d3b42f2b1137
Ivan Markovski <sagitariusim@gmail.com>
2024-06-12 11:20:16 +0200

    updates

--- internal/repository/migrations/000001_init_schema.up.sql
+++ internal/repository/migrations/000001_init_schema.up.sql
@@ -1,11 +1,59 @@
 create database schedules_data
     with owner postgres;
 
+create table public.schema_migrations
+(
+    version bigint  not null
+        primary key,
+    dirty   boolean not null
+);
+
+alter table public.schema_migrations
+    owner to postgres;
+
+create table public.admins
+(
+    id        bigserial
+        primary key,
+    user_name varchar(255) not null,
+    password  varchar(255) not null
+);
+
+alter table public.admins
+    owner to postgres;
+
+create table public.annual_leaves
+(
+    id            bigserial
+        primary key,
+    email         text,
+    position_name text,
+    start_date    date,
+    end_date      date
+);
+
+alter table public.annual_leaves
+    owner to postgres;
+
+create table public.schedules
+(
+    id         bigserial
+        primary key,
+    start_date timestamp with time zone,
+    end_date   timestamp with time zone
+);
+
+alter table public.schedules
+    owner to postgres;
+
 create table public.positions
 (
     id            bigserial
         primary key,
-    position_name text
+    position_name text,
+    position_id   bigint
+        constraint fk_schedules_positions
+            references public.schedules
 );
 
 alter table public.positions
@@ -45,29 +93,5 @@ create table public.shifts
 );
 
 alter table public.shifts
-    owner to postgres;
-
-create table public.admins
-(
-    id        bigserial
-        primary key,
-    user_name varchar(255) not null,
-    password  varchar(255) not null
-);
-
-alter table public.admins
-    owner to postgres;
-
-create table public.annual_leaves
-(
-    id            bigserial
-        primary key,
-    email         text,
-    position_name text,
-    start_date    date,
-    end_date      date
-);
-
-alter table public.annual_leaves
     owner to postgres;
 
