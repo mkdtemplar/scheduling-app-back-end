0147e8785dc78cc7f11261e583a8bcab91753845
Ivan Markovski <sagitariusim@gmail.com>
2024-06-16 17:39:18 +0200

    updates

--- internal/models/daily_schedule.go
+++ internal/models/daily_schedule.go
@@ -1,10 +1,14 @@
 package models
 
+import (
+	"github.com/jackc/pgx/pgtype"
+)
+
 type DailySchedule struct {
-	ID             int64        `gorm:"type:bigint;primaryKey" json:"id,string" binding:"required"`
-	StartDate      string       `json:"start_date" db:"start_date" gorm:"type:time"`
-	PositionsNames []string     `json:"positions_names" db:"positions" binding:"required" gorm:"type:text[]"`
-	Employees      []string     `json:"employees" db:"employees" binding:"required" gorm:"type:text[]"`
-	Shifts         []string     `json:"shifts" db:"shifts" binding:"required" gorm:"type:text[]"`
-	Positions      []*Positions `gorm:"foreignKey:PositionID;references:ID" json:"positions,omitempty"`
+	ID             int64            `gorm:"type:bigint;primaryKey" json:"id,string" binding:"required"`
+	StartDate      string           `json:"start_date" db:"start_date" gorm:"type:time"`
+	PositionsNames []string         `json:"positions_names" db:"positions" binding:"required" gorm:"type:text[]"`
+	Employees      pgtype.TextArray `json:"employees" db:"employees" binding:"required" gorm:"type:text[]"`
+	Shifts         pgtype.TextArray `json:"shifts" db:"shifts" binding:"required" gorm:"type:text[]"`
+	Positions      []*Positions     `gorm:"foreignKey:PositionID;references:ID" json:"positions,omitempty"`
 }
