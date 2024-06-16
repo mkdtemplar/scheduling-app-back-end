fdeb0cf128586db3ddfbc418b2f67801c1e022b5
Ivan Markovski <sagitariusim@gmail.com>
2024-06-16 22:08:38 +0200

    updates

--- internal/models/daily_schedule.go
+++ internal/models/daily_schedule.go
@@ -1,10 +1,12 @@
 package models
 
+import "github.com/lib/pq"
+
 type DailySchedule struct {
-	ID             int64        `gorm:"type:bigint;primaryKey" json:"id,string" binding:"required"`
-	StartDate      string       `json:"start_date" db:"start_date" gorm:"type:time"`
-	PositionsNames []string     `json:"positions_names" db:"positions" binding:"required" gorm:"type:text[]"`
-	Employees      []string     `json:"employees" db:"employees" binding:"required" gorm:"type:text[]"`
-	Shifts         []string     `json:"shifts" db:"shifts" binding:"required" gorm:"type:text[]"`
-	Positions      []*Positions `gorm:"foreignKey:PositionID;references:ID" json:"positions,omitempty"`
+	ID             int64          `gorm:"type:bigint;primaryKey" json:"id,string" binding:"required"`
+	StartDate      string         `json:"start_date" db:"start_date" gorm:"type:time"`
+	PositionsNames pq.StringArray `json:"positions_names" db:"positions_names" binding:"required" gorm:"type:text[]"`
+	Employees      pq.StringArray `json:"employees" db:"employees" binding:"required" gorm:"type:text[]"`
+	Shifts         pq.StringArray `json:"shifts" db:"shifts" binding:"required" gorm:"type:text[]"`
+	Positions      []*Positions   `gorm:"foreignKey:PositionID;references:ID" json:"positions,omitempty"`
 }
--- internal/models/dto/daily_scheduleDto.go
+++ internal/models/dto/daily_scheduleDto.go
@@ -1,9 +1,11 @@
 package dto
 
+import "github.com/lib/pq"
+
 type CreateDailyScheduleRequest struct {
-	ID             int64    `gorm:"type:bigint;primaryKey" json:"id,string" binding:"required"`
-	StartDate      string   `json:"start_date" db:"start_date" gorm:"type:time"`
-	PositionsNames []string `json:"positions_names" db:"positions" binding:"required" gorm:"type:text[]"`
-	Employees      []string `json:"employees" db:"employees" binding:"required" gorm:"type:text[]"`
-	Shifts         []string `json:"shifts" db:"shifts" binding:"required" gorm:"type:text[]"`
+	ID             int64          `gorm:"type:bigint;primaryKey" json:"id,string" binding:"required"`
+	StartDate      string         `json:"start_date" db:"start_date" gorm:"type:time"`
+	PositionsNames pq.StringArray `json:"positions_names" db:"positions_names" binding:"required" gorm:"type:text[]"`
+	Employees      pq.StringArray `json:"employees" db:"employees" binding:"required" gorm:"type:text[]"`
+	Shifts         pq.StringArray `json:"shifts" db:"shifts" binding:"required" gorm:"type:text[]"`
 }
